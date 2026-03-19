afc8fa7079fb60a12caace51c67b9e9364dec37a
david.hart <35079407+davehartx@users.noreply.github.com>
2026-03-19 09:33:51 +0000

    test1

--- 06/demos/After/form.php
+++ 06/demos/After/form.php
@@ -2,7 +2,7 @@
 <html>
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8">   <?-- test 
     <link rel="icon" href="img/favicon.ico">
     <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
